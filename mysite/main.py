ee8d1bb305fc7f14f6a89fb16ec187afa430202b
aiba2005 <sharshenbekovaitbek@gmail.com>
2026-01-08 15:07:57 +0600

    test_chat

---
+++ mysite/main.py
@@ -0,0 +1,22 @@
+import sys
+from pathlib import Path
+ROOT_DIR = Path(__file__).resolve().parent
+sys.path.append(str(ROOT_DIR.parent))
+from fastapi import FastAPI
+import uvicorn
+from starlette.middleware.sessions import SessionMiddleware
+from mysite.api import auth
+from mysite.config import SECRET_KEY
+from mysite.api import auth, group_people, chat_group, user
+
+app = FastAPI()
+
+app.include_router(auth.auth_router)
+app.include_router(user.user_router)
+app.include_router(chat_group.group_router)
+app.include_router(group_people.people_router)
+app.add_middleware(SessionMiddleware, secret_key=SECRET_KEY)
+
+
+if __name__ == '__main__':
+    uvicorn.run(app, host='127.0.0.1', port=8000)
